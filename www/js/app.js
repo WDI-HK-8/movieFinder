966c404fda34d6aeb506fa42a24181b2263ed9c6
Harry Chen <harry@harry.gg>
2015-08-31 03:22:23 +0800

    Add logging in/out + signing up

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ng-token-auth'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,7 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $authProvider) {
   $stateProvider
 
     .state('app', {
@@ -50,35 +50,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'MovieShowCtrl'
       }
     }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
   });
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/movies');
+
+  $authProvider.configure({
+    // By default, you only need to configure apiUrl
+    // Note that if you put a '/' at the end of the link, there will be errors when calling the api
+    apiUrl: 'http://localhost:3000'
+  })
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,58 +1,82 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $auth, $ionicPopup, $state) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  var validateUser = function(){
+    $scope.currentUser = JSON.parse(window.localStorage['current-user'] || '{}');
+    $scope.loggedIn = $scope.currentUser ? true : false
+    console.log('current user:', $scope.currentUser);
+  };
 
   // Form data for the login modal
   $scope.loginData = {};
 
-  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.loginModal = modal;
   });
 
-  // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginModal.hide();
   };
 
-  // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.loginModal.show();
   };
 
-  // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    $auth.submitLogin($scope.loginData)
+      .then(function(resp) {
+        window.localStorage['current-user'] = JSON.stringify(resp);
+        validateUser();
+        $scope.closeLogin();
+      })
+      .catch(function(resp) {
+        $ionicPopup.alert({
+          title: 'Wrong Email and Password',
+          template: 'Please try again'
+        });
+      });
   };
-})
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+  $scope.doLogout = function() {
+    console.log('Doing logout');
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+    window.localStorage['current-user'] = null;
+    validateUser();
+  };
+
+  // Form data for the signup modal
+  $scope.signupData = {};
+
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.signupModal = modal;
+  });
+
+  $scope.closeSignup = function() {
+    $scope.signupModal.hide();
+  };
+
+  $scope.signup = function() {
+    $scope.signupModal.show();
+  };
+
+  $scope.doSignup = function() {
+    console.log('Doing login', $scope.signupData);
+
+    $auth.submitRegistration($scope.signupData).
+      then(function(resp) {
+       console.log(resp);
+      }).
+      catch(function(resp) {
+       console.log(resp);
+      });
+  };
 })
 
 .controller('MovieShowCtrl', function($scope, $stateParams, $http) {
